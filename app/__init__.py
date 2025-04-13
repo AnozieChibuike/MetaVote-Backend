780cbc94c4b5a72dae7c29a2afe3dd46c9ad7f04
AnozieChibuike <chibuikeanozie0@gmail.com>
2025-04-13 20:49:53 +0100

    Backend Push

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate # type: ignore[import-untyped]
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
 from flasksqlalchemybasemodel import db
 from flask_cors import CORS
 from flask_mail import Mail
